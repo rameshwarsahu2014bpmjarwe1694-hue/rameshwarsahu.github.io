145db3966480dc9e502f65083d9768213f5d18fa
https://github.com/RS/Online/GDS/Departmental/Exam/Study/Hub. git <rameshwarsahu2014bpmjarwe1694@gmail.com>
2026-05-13 08:17:14 +0530

    Update sw.js
    Signed-off-by: https://github.com/RS/Online/GDS/Departmental/Exam/Study/Hub. git <rameshwarsahu2014bpmjarwe1694@gmail.com>

--- sw.js
+++ sw.js
@@ -1,6 +1,19 @@
 const CACHE_NAME = "gds-cache-v1";
 
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
 self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
   self.skipWaiting();
 });
 
@@ -9,5 +22,9 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
